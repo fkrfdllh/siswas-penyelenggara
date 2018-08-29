4dbf2fe8f9ab25531bb9332375f369f7b8f74124
soffandluffy <soffandluffy@gmail.com>
2018-08-29 14:20:11 +0700

    Adding materialize login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,67 +2,36 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="section"></div>
+        <div class="section"></div>
+        <div class="col s6 offset-s3 m6 offset-m3 l6 offset-l3">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                <div class="card-image">
+                  <img src="{{ asset('images/intersoft.jpg')}}" width="100%" height="100%">
+                  <span class="card-title center">Login</span>
+                </div>
+                <div class="card-content">
+                    <form method="" action="#">
+                        <div class="row">
+                                <div class="input-field col s10 offset-s1">
+                                    <i class="material-icons prefix">account_circle</i>
+                                    <input id="icon_prefix1" type="text" class="validate" required autofocus></input>
+                                    <label for="icon_prefix1">Username</label>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
+                                <div class="input-field col s10 offset-s1">
+                                    <i class="material-icons prefix">lock</i>
+                                    <input id="icon_prefix2" type="password" class="validate" required></input>
+                                    <label for="icon_prefix2">Password</label>
+                                </div>
+                                <div class="input-field col s10 offset-s1">
+                                    <button class="col s12 waves-effect waves-dark btn light-blue">SIGN IN</button> 
+                                </div>
                         </div>
                     </form>
+                </div>
+                <div class="card-action right-align">
+                  <a href="#">Lupa Password</a>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,36 +11,43 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/materialize.min.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/materialize.min.css') }}" rel="stylesheet">
+
+    <style type="text/css">
+        body {
+            background-color: #ECEFF1;
+        }
+    </style>
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+        <!-- <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container"> -->
+                <!-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
+                <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    Left Side Of Navbar
                     <ul class="navbar-nav mr-auto">
 
-                    </ul>
+                    </ul> -->
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                     <!-- <ul class="navbar-nav ml-auto"> -->
                         <!-- Authentication Links -->
-                        @guest
+                        <!-- @guest
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -65,13 +72,14 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                        @endguest -->
+                    <!-- </ul>  -->
 
-        <main class="py-4">
+                <!-- </div> -->
+            <!-- </div> -->
+        <!-- </nav> -->
+
+        <main class="py-4 ">
             @yield('content')
         </main>
     </div>
